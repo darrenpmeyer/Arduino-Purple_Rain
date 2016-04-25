1bd02b9a5dfbdf2fde32017665059d96f78ee165
Darren P Meyer <darren@darrenpmeyer.com>
2016-04-25 10:44:43 -0500

    Source code (c) and license notice

--- Purple_Rain.ino
+++ Purple_Rain.ino
@@ -1,3 +1,4 @@
+/* Copyright (C) 2016 Darren P Meyer, LGPL 2.1 (see bottom) */
 #include <FastLED.h> // see http://fastled.io; uses branch FastLED3.1
 
 // Configure LED driver
@@ -72,3 +73,20 @@ void loop() {
     FastLED.show();
     FastLED.delay(DECAY_DELAY);
 }
+
+/*
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+    USA
+*/
